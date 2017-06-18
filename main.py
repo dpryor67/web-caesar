c38fa569d8cac7057686bdde957a5a183b1359a1
dpryor <santonix67@hotmail.com>
2017-06-17 18:07:37 -0700

    return encrypted string via form

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
-from flask import flask
+from flask import Flask, request
+from caesar import rotate_string
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 form = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form action="/" method="post">
+            <label for="rot">Rotate by:</label>
+            <input type="text" name="rot" value="0">
+            <textarea name="text"></textarea>
+            <button type="submit">Submit query</button>
+        </form>
+
+    </body>
+</html>
 
 """
 
 
 @app.route("/")
 def index():
-    return "Hello World"
+    return form
+
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form.get('rot'))
+    text = request.form.get('text')
+    encrypted_string = rotate_string(text, rot)
+    return '<h1>{encrypted_string}</h1>'.format(encrypted_string=encrypted_string)
 
 app.run()
